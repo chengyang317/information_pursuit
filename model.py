1b6d48d6aa58f62f6c6f50299bd7848bef077013
chengyang317 <chengyang317@gmail.com>
2016-04-12 14:01:31 -0700

    update

--- model.py
+++ model.py
@@ -1,3 +1,19 @@
+import net
+import tensorflow as tf
+import dataset
+
+
+class InforModel(object):
+
+    def __init__(self):
+        self.train_path = '/scratch/dataset/information_pursue'
+        self.dataset_path = '/scratch/dataset/information_pursue'
+        self.images_path = '/scratch/dataset/256_ObjectCategories'
+        self.dataset = dataset.DataSet()
+
+    def create_dataset(self):
+
+
 
 
 
