7b16f0b219d72f21cae0f88fc0edef247c7170c3
chengyang317 <chengyang317@gmail.com>
2016-04-15 13:32:27 -0700

    update

--- conv_net.py
+++ conv_net.py
@@ -3,6 +3,7 @@ import framwork
 import os
 import tensorflow as tf
 from net import Net
+import layer
 
 
 class ConvNet(Net):
@@ -10,33 +11,17 @@ class ConvNet(Net):
         super(ConvNet, self).__init__(net_name, batch_size, net_percent, work_path, data_set)
         self.net_params = {'weight_decay': 0.1, 'learning_rate': 0.1, 'train_loops': 10000,
                            'devices': ['/cpu:0', '/gpu:0', '/gpu:1', '/gpu:2']}
-        self.layer_attrs = {'input_layer': {'layer_name': 'input', 'shape': (self.batch_size, ) + self.image_shape},
-                            'conv1_layer': {'layer_name': 'conv1', 'shape': [11, 11, 3, int(96 * self.net_percent)],
-                                            'stddev': 1e-2, 'strides': [1, 4, 4, 1], 'padding': 'SAME', 'biase': 0.1},
-                            'norm2_layer': {'layer_name': 'norm2', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4,
-                                            'beta': 0.75},
-                            'pool3_layer': {'layer_name': 'pool3', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
-                                            'padding': 'SAME'},
-                            'conv4_layer': {'layer_name': 'conv4',
-                                            'shape': [5, 5, int(96 * self.net_percent), int(256 * self.net_percent)],
-                                            'stddev': 1e-2, 'strides': [1, 2, 2, 1], 'padding': 'SAME', 'biase': 0.1},
-                            'norm5_layer': {'layer_name': 'norm5', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4,
-                                            'beta': 0.75},
-                            'pool6_layer': {'layer_name': 'pool6', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
-                                            'padding': 'SAME'},
-                            'conv7_layer': {'layer_name': 'conv7',
-                                            'shape': [3, 3, int(256 * self.net_percent), int(256 * self.net_percent)],
-                                            'stddev': 1e-2, 'strides': [1, 2, 2, 1], 'padding': 'SAME', 'biase': 0.1},
-                            'norm8_layer': {'layer_name': 'norm8', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4,
-                                            'beta': 0.75},
-                            'pool9_layer': {'layer_name': 'pool9', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
-                                            'padding': 'SAME'},
-                            }
+        self.layer_attrs = layer.define_ConvNet_layers(shape=(self.batch_size,) + self.image_shape, net_percent= net_percent)
 
         self.net_device = self.net_params['devices'][3]
 
     def build_network(self):
         with self.net_graph.as_default(), tf.device(self.net_device):
+
+
+
+
+
             self.build_placeholders()
             self.build_logits(self.net_tensors['images'])
             self.build_loss(self.net_tensors['logits'], self.net_tensors['labels'])
--- layer.py
+++ layer.py
@@ -1,20 +1,23 @@
-
-
 def define_ConvNet_layers(shape, net_percent):
     layers_attrs = {'input_layer': {'layer_name': 'input', 'shape': shape},
                     'conv1_layer': {'layer_name': 'conv1', 'shape': [11, 11, 3, int(96 * net_percent)], 'stddev': 1e-2,
                                     'strides': [1, 4, 4, 1], 'padding': 'SAME', 'biase': 0.1},
+                    'norm1_layer': {'layer_name': 'norm1', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4, 'beta': 0.75},
+                    'pool1_layer': {'layer_name': 'pool1', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
+                                    'padding': 'SAME'},
+                    'conv2_layer': {'layer_name': 'conv2', 'shape': [5, 5, int(96 * net_percent), int(256 * net_percent)],
+                                    'stddev': 1e-2, 'strides': [1, 2, 2, 1], 'padding': 'SAME', 'biase': 0.1},
                     'norm2_layer': {'layer_name': 'norm2', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4, 'beta': 0.75},
+                    'pool2_layer': {'layer_name': 'pool2', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
+                                    'padding': 'SAME'},
+                    'conv3_layer': {'layer_name': 'conv3', 'shape': [3, 3, int(256 * net_percent), int(256 * net_percent)],
+                                    'stddev': 1e-2, 'strides': [1, 2, 2, 1], 'padding': 'SAME', 'biase': 0.1},
+                    'norm3_layer': {'layer_name': 'norm3', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4, 'beta': 0.75},
                     'pool3_layer': {'layer_name': 'pool3', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
                                     'padding': 'SAME'},
-                    'conv4_layer': {'layer_name': 'conv4', 'shape': [5, 5, int(96 * net_percent), int(256 * net_percent)],
-                                    'stddev': 1e-2, 'strides': [1, 2, 2, 1], 'padding': 'SAME', 'biase': 0.1},
-                    'norm5_layer': {'layer_name': 'norm5', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4, 'beta': 0.75},
-                    'pool6_layer': {'layer_name': 'pool6', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
-                                    'padding': 'SAME'},
-                    'conv7_layer': {'layer_name': 'conv7', 'shape': [3, 3, int(256 * net_percent), int(256 * net_percent)],
-                                    'stddev': 1e-2, 'strides': [1, 2, 2, 1], 'padding': 'SAME', 'biase': 0.1},
-                    'norm8_layer': {'layer_name': 'norm8', 'depth_radius': 5, 'bias': 1.0, 'alpha': 1e-4, 'beta': 0.75},
-                    'pool9_layer': {'layer_name': 'pool9', 'ksize': [1, 3, 3, 1], 'strides': [1, 2, 2, 1],
-                                    'padding': 'SAME'},
+                    'full4_layer': {'layer_name': 'full4', 'shape': [None, int(4096 * net_percent)], 'stddev': 1e-2,
+                                    'biase': 0.1},
+                    'softmax_layer': {'layer_name': 'softmax1', 'shape': [int(4096 * net_percent, None)],
+                                      'stddev': 1e-2, 'biase': 0.1}
                     }
+    return layers_attrs
